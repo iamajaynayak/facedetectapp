1775ed3f4ea3a5f5a217349221e246af8a150c00
iamajaynayak <63513168+iamajaynayak@users.noreply.github.com>
2020-07-31 17:50:56 +0530

    Updates

---
+++ precache-manifest.b020296477e62f124e912d5bf311f28f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "574917e43406aca5c59e276d596891c6",
+    "url": "/facedetectapp/index.html"
+  },
+  {
+    "revision": "db69092978986c0900a0",
+    "url": "/facedetectapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b496b8df8759de9135eb",
+    "url": "/facedetectapp/static/css/main.5fcadf66.chunk.css"
+  },
+  {
+    "revision": "db69092978986c0900a0",
+    "url": "/facedetectapp/static/js/2.f7ac846c.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facedetectapp/static/js/2.f7ac846c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b496b8df8759de9135eb",
+    "url": "/facedetectapp/static/js/main.4234b164.chunk.js"
+  },
+  {
+    "revision": "c53458b6fd48fe38a898",
+    "url": "/facedetectapp/static/js/runtime-main.7216d377.js"
+  }
+]);

df2232feda634f55f907852f51f23e8a282e9184
iamajaynayak <63513168+iamajaynayak@users.noreply.github.com>
2020-08-08 12:54:05 +0530

    editing footer file

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
 	<div className="mt5">
-	<p className="f4">{'This App was bulid using react'}</p>
+	<p className="f4">{'This App was build using react'}</p>
 	<p>{`iamajaynayak@2020`}</p>
 	</div>
 	)

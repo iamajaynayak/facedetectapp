0e695c2485885bd6adbe3a610cb6fda6aba95896
iamajaynayak <63513168+iamajaynayak@users.noreply.github.com>
2020-08-08 13:02:28 +0530

    adding edited footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => (
-	<div className="mt5">
-	<p className="f4">{'This App was build using react'}</p>
-	<p>{`iamajaynayak@2020`}</p>
-	</div>
-	)
+  <div className="mt5">
+    <p className="f4">{"This App was build using react"}</p>
+    <p>{`iamajaynayak@2020`}</p>
+  </div>
+);
 
-export default Footer
+export default Footer;

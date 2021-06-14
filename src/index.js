d86154038acf140b62330f864e73c6209722a43c
Ajay Nayak <63513168+iamajaynayak@users.noreply.github.com>
2021-06-14 20:57:23 +0530

    Update index.js
    pwa

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
 
@@ -17,3 +17,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
+serviceWorker.register();
